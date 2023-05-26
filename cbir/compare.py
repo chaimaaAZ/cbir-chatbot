88d580d69dc13e1f60e0a5123c1775486ae9e82c
chaimaa azziz <chaimaa_azziz@um5.ac.ma>
2023-05-26 13:22:16 +0000

    updated folder structure

--- cbir/compare.py
+++ cbir/compare.py
@@ -1,27 +1,28 @@
 import numpy as np
 import cv2
 import json 
-from scipy.spatial import distance
+import scipy.spatial.distance as dist
 
 class Compare :
     def __init__(self,indexpath) :
         self.indexpath=indexpath
     # Calculate the Euclidean distance
     def eucledien_distance(self,a,b) :
-       distance = distance.euclidean(a,b)
+       distance = dist.euclidean(a,b)
        return distance 
     
     
     def search(self,query_feature) :
         
-        with open('index.json', 'r') as file:
+        with open(self.indexpath, 'r') as file:
           data = json.load(file)
-          for dictio in data :
-            distance["image"]=0
-            for i in range(len(data["image"]["colors"])) :
-               distance["image"]+=0.5*self.eucledien_distance(data["colors"][i],query_feature["colors"][i]) 
-               distance["image"]+=0.25*self.eucledien_distance(data["texture"][i],query_feature["texture"][i]) 
-               distance["image"]+=0.25*self.eucledien_distance(data["shape"][i],query_feature["shape"][i])
+          distance=dict()
+          for key in data :
+            distance[key]=0
+            distance[key]+=0.5*self.eucledien_distance(data[key]["colors"],query_feature["colors"]) 
+               
+            distance[key]+=0.25*self.eucledien_distance(data[key]["texture"],query_feature["texture"]) 
+            distance[key]+=0.25*self.eucledien_distance(data[key]["shape"],query_feature["shape"])
         
         similar_images = sorted(distance.items(), key=lambda x: x[1])
         return similar_images[:10]           
--- cbir/search.py
+++ cbir/search.py
@@ -6,23 +6,22 @@ from descriptors import descriptor
 d=descriptor((4,4,4))
 
 
-image ='C:\\Users\\Chaimaa\\Documents\\iphone\\pictures\\2022_11_18_19_35_IMG_7632.jpg'
+image ='C:\\Users\\Chaimaa\\Documents\\iphone\\pictures\\2022_06_28_14_56_IMG_3167.jpg'
 query=cv2.imread(image)
 query=cv2.resize(query,(300,400))
 #perform the search
 features = dict()
-features["Color"] = d.describe(query,"histogram_hsv")
-features["Texture"] = d.describe(query,"histogram_lbp")
-features["Shape"] = d.describe(query,"histogram_sobel")
+features["colors"] = d.describe(query,"histogram_hsv")
+features["texture"] = d.describe(query,"histogram_lbp")
+features["shape"] = d.describe(query,"histogram_sobel")
 searcher= Compare('index.json')
 
 results=searcher.search(features)
 
 cv2.imshow("query",query)
 
-for (score,result_image) in results :
+for (result_image,score) in results :
     #load the result images and display them
-    
     result=cv2.imread(result_image)
     result=cv2.resize(result,(300,400))
     cv2.imshow("result",result)
