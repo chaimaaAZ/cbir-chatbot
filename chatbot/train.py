b5c3902eba9a5cd016373078ca492083dce9cbb0
chaimaa azziz <chaimaa_azziz@um5.ac.ma>
2023-05-31 01:47:27 +0000

    introducing NLP

--- chatbot/train.py
+++ chatbot/train.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from model import NeuralNet
 
 
 with open(r"C:\Users\Chaimaa\Documents\data\python\image search engine 2\chatbot\training.json","r") as f:
@@ -57,5 +58,59 @@ class ChatDataset(Dataset) :
         return self.n_samples
 #hyper parameters
 batch_size=8
+hidden_size=8
+output_size=len(tags)
+input_size=len(X_train[0])
+num_epochs = 1000
+learning_rate = 0.001
+
+
+
+
+
 dataset=ChatDataset()
 train_loader=DataLoader(dataset=dataset,batch_size=batch_size,shuffle=True,num_workers=0)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train the model
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
+        
+        # Forward pass
+        outputs = model(words)
+        # if y would be one-hot, we must apply
+        # labels = torch.max(labels, 1)[1]
+        loss = criterion(outputs, labels)
+        
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    if (epoch+1) % 100 == 0:
+        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+
+print(f'final loss: {loss.item():.4f}')
+
+data = {
+"model_state": model.state_dict(),
+"input_size": input_size,
+"hidden_size": hidden_size,
+"output_size": output_size,
+"all_words": all_words,
+"tags": tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f'training complete. file saved to {FILE}')
